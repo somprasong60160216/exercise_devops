bb34e43137c2476711416f24095056be0832cdaf
somprasong60160216 <60160216@go.buu.ac.th>
2020-11-08 21:41:36 +0700

    first commit

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+const request = require('supertest')
+const app = require('../index')
+
+describe('Validate setting the test', () => {
+    it('should be ok', () => {
+        assert.ok(true)
+    })
+})
